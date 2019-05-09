3f7ad5c5154ca8db47ba73a1b5a59bc5535bc18b
jefshi <jefshi@yeah.net>
2019-05-09 11:49:39 +0800

    [formwork-lib] init module: view-xxx

---
+++ view-xxx/src/main/java/com/csp/view/xxx/CustomView.java
@@ -0,0 +1,25 @@
+package com.csp.view.xxx;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+//    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//        super(context, attrs, defStyleAttr, defStyleRes);
+//    }
+}
